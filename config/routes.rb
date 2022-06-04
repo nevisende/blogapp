2a26a7431a48add2238b7462e6ca8247d131db46
nevisende <ekonomitakipmail@gmail.com>
2022-06-04 07:23:53 +0300

    Feat: make users page as a route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
+  root "users#index"
   resources :user, only: [:index, :show]do
     resources :posts, only: [:index, :show]
   end
