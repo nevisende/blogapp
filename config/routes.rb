701966f4f4bef81ac8de5d34b8a0d12761c6905b
nevisende <ekonomitakipmail@gmail.com>
2022-06-11 14:12:31 +0300

    Feat: recognize new pages to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :user, only: [:index, :show]do
     resources :posts, only: [:index, :show]
   end
+  get "/posts/new", to: "posts#new"
+  post "/posts/create", to: "posts#create"
+  post "/users/:user_id/posts/:id/like", to: "likes#create"
+  post "/users/:user_id/posts/:id/create_comment", to: "comments#create"
 end
