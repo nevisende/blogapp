57106d100574c9ae6ff361c384dc1ca2ac7df6a4
nevisende <ekonomitakipmail@gmail.com>
2022-06-11 14:08:36 +0300

    Feat: update post controller for the form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-   def index
+  def index
     @user = User.find(params[:user_id])
   end
 
@@ -7,5 +7,25 @@ class PostsController < ApplicationController
     @user = User.where(id: params[:user_id])[0]
     @post = @user.posts.where(id: params[:id])[0]
     @posts = Post.where(author_id: params[:user_id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to "#{users_path}/#{current_user.id}" }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:author_id, :title, :text)
   end
 end
