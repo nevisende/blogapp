23bcf86bbe9704d1c506e6f3c5ab10a92d68dcd2
nevisende <ekonomitakipmail@gmail.com>
2022-06-11 07:32:26 +0300

    Feat: add refs to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_04_065300) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_11_042832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_04_065300) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -26,6 +30,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_04_065300) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
@@ -37,6 +45,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_04_065300) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -49,7 +59,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_04_065300) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users"
+  add_foreign_key "posts", "users", column: "author_id"
 end
