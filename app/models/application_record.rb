6ea689641a3fd86b10559f262d80fe226f320cb7
nevisende <ekonomitakipmail@gmail.com>
2022-06-11 07:32:04 +0300

    Feat: add ıpdate_likes_counter method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
-  primary_abstract_class
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  belongs_to :post
+
+  def update_likes_counter
+    post.increment!(:likes_counter)
+    post.save
+  end
 end
