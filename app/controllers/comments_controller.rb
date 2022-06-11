3cdb51dced5413b515fcfe674cb6276849552eed
nevisende <ekonomitakipmail@gmail.com>
2022-06-11 14:07:37 +0300

    Feat: create comment controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,17 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = current_user.comments.new(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to request.path, params: { success: true } }
+      else
+        format.html { redirect_to request.path, params: { success: false } }
+      end
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:author_id, :post_id, :text)
+  end
+end
