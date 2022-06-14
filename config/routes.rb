f160835703ef7d4303ea6c7ec64c6cbc0225bbd3
nevisende <ekonomitakipmail@gmail.com>
2022-06-14 22:56:37 +0300

    Fix: users not being able to register and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
-   devise_for :users
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "users#index"
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:create]
-      resources :likes, only: [:create]
+
+  # Defines the root path route ("/")
+  devise_scope :user do
+    authenticated :user do
+      root :to => "users#show", as: :authenticated_root
+    end
+    unauthenticated :user do
+      root :to => "devise/sessions#new", as: :unauthenticated_root
     end
   end
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
+  get "/posts/new", to: "posts#new"
+  post "/posts/create", to: "posts#create"
+  post "/users/:user_id/posts/:id/like", to: "likes#create"
+  post "/users/:user_id/posts/:id/create_comment", to: "comments#create"
 end
