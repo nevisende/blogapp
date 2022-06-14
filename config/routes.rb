047372166ffeb3e9a7a390fcc3b864308cdc114a
nevisende <ekonomitakipmail@gmail.com>
2022-06-14 09:23:08 +0300

    Feat: add devise pages to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+   devise_for :users
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "users#index"
-  resources :user, only: [:index, :show]do
-    resources :posts, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:create]
+      resources :likes, only: [:create]
+    end
   end
-  get "/posts/new", to: "posts#new"
-  post "/posts/create", to: "posts#create"
-  post "/users/:user_id/posts/:id/like", to: "likes#create"
-  post "/users/:user_id/posts/:id/create_comment", to: "comments#create"
 end
